04333ee9355f78d436b209f28b71dafad2a1d5a2
jihee8138 <jihee8138@gmail.com>
2024-01-09 16:43:07 +0900

    240109

---
+++ 06_class/src/pkg01_object/Calculator.java
@@ -0,0 +1,13 @@
+package pkg01_object;
+
+public class Calculator {
+  
+  // 데이터 저장용 변수 -> 필드(field)
+  int number;
+  
+  // 기능 -> 메소드 (method)
+  void method() {
+    System.out.println("method");
+  }
+
+}
